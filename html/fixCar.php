706fc3d2d09f5fd0b3962721418bee70f6873625
robm3794 <38939394+robm3794@users.noreply.github.com>
2020-08-07 11:22:55 +0200

    Update fixCar.php

--- html/fixCar.php
+++ html/fixCar.php
@@ -1,13 +1,11 @@
 <?php
 include "header.php";
-?>
 
 <div id="maininfo">Fixing connection to the car, please wait for the dashcam icon to appear on the car.  <br> <p>
 
-<?php system("/home/pi/dash/connectCar"); ?>
+system("/home/pi/dash/connectCar");
 
 </div>
 
-<?php
 include "footer.php";
 ?>
