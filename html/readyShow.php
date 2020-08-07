952312141248cbf686f281557e7ece749b24d089
robm3794 <38939394+robm3794@users.noreply.github.com>
2020-08-07 11:22:08 +0200

    Update readyShow.php

--- html/readyShow.php
+++ html/readyShow.php
@@ -1,35 +1,32 @@
 <?php
 include "header.php";
-?>
 
 <div id="maininfo">Disconnecting the drive from the car...
 
-<?php
 exec("/home/pi/dash/disconnectCar", $output1, $ret1);
 if ($ret1 == 0) {
-?>	<font color='green'>done</font>.
-<?php } else {
-?>	<font color='red'>FAILED</font>:<br> <p> <pre>
-<?php	echo implode("\n", $output1);
-?>	</pre>
-<?php } ?>
+	<font color='green'>done</font>.
+	} else {
+		<font color='red'>FAILED</font>:<br> <p> <pre>
+		echo implode("\n", $output1);
+		</pre>
+		}
 
 <br> <p> Mounting the drive on the Pi...
-<?php
+
 exec("/home/pi/dash/connectPi", $output2, $ret2);
 if ($ret2 == 0) {
-?>	<font color='green'>done</font>.
-<?php } else {
-?>	<font color='red'>FAILED</font>:<br> <p> <pre>
-<?php	echo implode("\n", $output2);
-?>	</pre>
-<?php }
+	<font color='green'>done</font>.
+	} else {
+		<font color='red'>FAILED</font>:<br> <p> <pre>
+		echo implode("\n", $output2);
+		</pre>
+		}
 
 if (($ret1 == 0) && ($ret2 == 0)) {
-?>	<br> <p> <form action='videoList.php'> <button type='submit'>Show video list</button> </form>
-<?php } ?>
-</div>
+	<br> <p> <form action='videoList.php'> <button type='submit'>Show video list</button> </form>
+	}
+	</div>
 
-<?php
 include "footer.php";
 ?>
